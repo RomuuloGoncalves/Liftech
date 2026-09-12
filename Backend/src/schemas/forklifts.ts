de2afc26d2d59a87ad9e00b79f95f5aacccb9593
Rômulo Gonçalves <romulogoncalves13@gmail.com>
2026-09-11 22:35:59 -0300

    feat: add forklift and incident schemas for database modeling

---
+++ Backend/src/schemas/forklifts.ts
@@ -0,0 +1,17 @@
+import { conn } from "../config/conn.js"
+const { Schema } = conn.mongoose
+
+const forkliftSchema = new Schema({
+    identificacao: String,
+    dispositivo_conectado: {
+        type: conn.mongoose.Schema.Types.ObjectId,
+        ref: 'device'
+    },
+    operador_conectado: {
+        type: conn.mongoose.Schema.Types.ObjectId,
+        ref: 'operator'
+    }
+})
+
+export const Forklift = conn.mongoose.model('forklift', forkliftSchema)
+
---
+++ Backend/src/schemas/incidents.ts
@@ -0,0 +1,12 @@
+import { conn } from "../config/conn.js"
+const { Schema } = conn.mongoose
+
+    const incidentSchema = new Schema ({
+        data_incidente: Date,
+        forcao_impacto: Number,
+        status: String,
+        
+        
+    })
+
+const Incident = conn.mongoose.model('incident', incidentSchema)
